9448c357b0e710a0ec4cdf43ec3bfaf80aadadd4
TimShoup <1timothyshoup@gmail.com>
2025-06-24 21:16:23 -0700

    bookbot semifinished

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import count_words
 from stats import count_letters
+from stats import new_dicts
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,8 +10,15 @@ def get_book_text(filepath):
 
 def main():
     write = get_book_text("books/frankenstein.txt")
-    print(f"{count_words(write)} words found in the document")
-    print(count_letters(write))
-    print()
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(write)} total words")
+    print("--------- Character Count -------")
+    filter = new_dicts(count_letters(write))
+    for dic in filter:
+        if dic["char"].isalpha():
+            print(f"{dic['char']}: {dic['num']}")
+    print("============= END ===============")
     
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,16 @@ def count_letters(text):
         else:
             dict[letter] += 1
     return dict
+
+def new_dicts(dictionary):
+    new_list =[]
+    for character in dictionary:
+        new_dict = {"char": character, "num": dictionary[character]}
+        new_list.append(new_dict)
+
+    def sort_dict(single_dict):
+        return single_dict["num"]
+        
+    new_list.sort(reverse=True, key=sort_dict)
+
+    return new_list
